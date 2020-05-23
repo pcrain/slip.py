196c9a0df464fe9904e936cc2cebbfae5c6317e1
Patrick Crain <patrickacrain@gmail.com>
2020-05-23 01:51:16 -0500

    Updated Windows installation batch file to point to new common run script

--- _install-win.bat
+++ _install-win.bat
@@ -1,4 +1,4 @@
 SET PATH="%PATH%%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python38\Scripts;"
 %HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python38\Scripts\pip.exe install --user .
-mklink "%HOMEDRIVE%%HOMEPATH%\Desktop\slippi_viz.bat" "%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Python\Python38\site-packages\slippi_viz\_run-win.bat"
+mklink "%HOMEDRIVE%%HOMEPATH%\Desktop\slippi_viz.py" "%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Python\Python38\site-packages\slippi_viz\_run.py"
 PAUSE
