2ed3cd8d3aded90ddb80a81c2a2e08a1ae8120c0
Patrick Crain <patrickacrain@gmail.com>
2020-10-08 00:59:33 -0500

    Added stock and CSS silhouettes for missing character costumes in case of stock icon glitches; updated replay thumbnails to handle cases where player has more than 4 stocks; updated setup.py to read version information from the site config.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 from setuptools import setup
+import os
+
+#Get version from slip.py's config file
+def get_version():
+  with open(os.path.join("slippi_viz","app","config.py"),'r') as fin:
+    for line in [f for f in fin.readlines() if "SITE_VERSION" in f]:
+      return line.split('"')[1]
 
 setup(
     name                 = 'slippi_viz',
-    version              = '0.3.1',
+    version              = get_version(),
     packages             = ["slippi_viz"],
     include_package_data = True,
     zip_safe             = False,
